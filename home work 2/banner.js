2ee958cac9058a2695805a55918167c4a8c3bfe6
soheil <soheilsadabadi78@yahoo.com>
2012-07-30 11:03:00 +0330

    finished but not working

--- home work 2/banner.js
+++ home work 2/banner.js
@@ -3,10 +3,14 @@ window.onload = function(){
 	var lists = slide.getElementsByClassName('btns').item(0).getElementsByTagName('li');
 	var currentSlide = 0
 	gotoslide = function(n){
+		if(n>lists.length-1) n=0;
 		train.style.left=(-450*n) + 'px';
 		lists.item(currentSlide).className = '';
 		lists.item(n).className = 'active';
 		currentSlide=n;
+	}
+	nextSlide = function(){
+		go2slide(currentSlide+1);
 	}
 	for (var i = 0; i < lists.length; i++) {
 		function = tempo(){
@@ -16,5 +20,5 @@ window.onload = function(){
 		}
 		tempo(i);
 	}
-
+		setInterval(nextSlide,500);
 }
