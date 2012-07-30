09ac666fdb29e32c4f59fb3a57974c3ccd7e40e2
soheil <soheilsadabadi78@yahoo.com>
2012-07-30 10:53:25 +0330

    most is done. lets auto play !

--- home work 2/banner.js
+++ home work 2/banner.js
@@ -1,13 +1,16 @@
 window.onload = function(){
 	var slide = document.getElementsByClassName('slide').item(0);
 	var lists = slide.getElementsByClassName('btns').item(0).getElementsByTagName('li');
+	gotoslide = function(n){
+		train.style.left=(-450*n) + 'px';
+	}
 	for (var i = 0; i < lists.length; i++) {
 		function = tempo(){
 			lists.item(j).onclick=function(){
 			alert(j);
 			}
 		}
-		tmp(i);
+		tempo(i);
 	}
 
 }
