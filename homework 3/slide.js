3330c22c3a0be14fe104cc7f07fc826163a63b10
soheil <soheilsadabadi78@yahoo.com>
2012-08-01 10:50:23 +0330

    finished completly

--- homework 3/slide.js
+++ homework 3/slide.js
@@ -1,7 +1,41 @@
 window.onload=function(){
-	var slide= document.getElementsByClassName('slide');
-	var buttons = slide.getElementsByClassName('btns').item(0).getElementsByTagName('li');
+	var slide= document.getElementsByClassName('slide').item(0);
+	var btns = slide.getElementsByClassName('btns').item(0).getElementsByTagName('li');
 	var train = slide.getElementsByClassName('train').item(0);
-	var 
+	currentSlide = 0;
+
+	(goToSlide = function(n) {
+		if(n>btns.length-1) n=0;
+		if (n<0) n=btns.length-1;
+		train.style.left=(-450*n)+'px';
+		btns.item(currentSlide).className = ''
+		btns.item(n).className = 'active';
+		currentSlide=n;
+	})(0)
+	nextSlide=function(){
+		goToSlide(currentSlide+1)
+	}
+	prvSlide=function(){
+		goToSlide(currentSlide-1)
+	}
+	var autoPlayIv = false;
+	(autoPlayStart = slide.onmouseout = function(){
+		if(autoPlayIv) return;
+		autoPlayIv = setInterval(nextSlide, 3000);
+	})(); 
+
+	autoPlayStop = slide.onmouseover = function(){
+		clearInterval(autoPlayIv);
+		autoPlayIv = false;
+	}
+	slide.getElementsByClassName('next').item(0).onclick=nextSlide;
+	slide.getElementsByClassName('previous').item(0).onclick=prvSlide;
+	for (var i = 0; i < btns.length; i++) {
+		(function(j){
+			btns.item(j).onclick= function(){
+				goToSlide(j);
+			}
+		})(i);
+	}
 
 }
