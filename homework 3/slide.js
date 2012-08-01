235d0e9c697e49692c1ebbe28498512cd266a8a5
soheil <soheilsadabadi78@yahoo.com>
2012-08-01 10:21:20 +0330

    for now ...

---
+++ homework 3/slide.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+	var slide= document.getElementsByClassName('slide');
+	var buttons = slide.getElementsByClassName('btns').item(0).getElementsByTagName('li');
+	var train = slide.getElementsByClassName('train').item(0);
+	var 
+
+}
